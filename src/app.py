3bfe44d101077bd3669f09ada019aaaddac12c3c
i30101 <andrewkim229@gmail.com>
2024-08-29 09:45:05 -0400

    app

---
+++ src/app.py
@@ -0,0 +1,54 @@
+"""
+Andrew Kim
+
+27 June 2025
+
+Version 1.0.0
+
+CrystalEyes: A Python application for analyzing images of nano-ice crystals.
+This app automates the extraction of ice crystal features, such as size and frequency
+from images of a sample taken over a period of time.
+
+This app supports Linkam Data Files in the .ldf format.
+Individual frames or a series of frames can be analyzed by the app.
+
+The machine learning algorithm is powered by Cellpose, a software designed to identify cytoplasm from cells.
+Using a graphics card is highly recommended to accelerate the Cellpose algorithm.
+"""
+
+
+
+import tkinter as tk
+
+from gui import Gui
+from splash import Splash
+from variables import Variables
+
+
+
+def main():
+    """ Starts the app by running the splash screen """
+
+    root = tk.Tk()
+
+    # set app icon for the splash window
+    icon = tk.PhotoImage(file=Variables.ICON_PATH, master=root)
+    root.iconphoto(False, icon)
+
+    splash = Splash(root)
+
+    start()
+
+
+def start():
+    window = tk.Tk()
+
+    # set up app icon for main window
+    icon = tk.PhotoImage(file=Variables.ICON_PATH, master=window)
+    window.iconphoto(False, icon)
+
+    gui = Gui(window)
+
+
+if __name__ == '__main__':
+    main()
